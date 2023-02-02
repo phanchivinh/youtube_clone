c0ad6cd26179a1365e9b9009d7ee78661855b5f9
Vinh Phan <phanchivinh.7275@gmail.com>
2023-02-02 20:04:30 +0700

    start fetch API from rapidAPI

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,12 +1,26 @@
-// import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { Stack, Box, Typography } from '@mui/material'
-import Sidebar from './Sidebar'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
+
+import { Sidebar, Videos } from './'
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState('New')
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    console.log(selectedCategory);
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then(data => setVideos(data.items))
+  }, [selectedCategory])
+
   return (
     <Stack sx={{ flexDirection: { xs:'column', md:'row' }}}>
         <Box sx={{ height: { xs: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
-            <Sidebar />
+            <Sidebar 
+                selectedCategory={selectedCategory} 
+                setSelectedCategory={setSelectedCategory}
+                />
 
             <Typography className='copyright' variant='body2' sx={{ color:'#fff' }}>
                 Copyright 2023 JSM Media
@@ -15,10 +29,10 @@ const Feed = () => {
 
         <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
             <Typography variant='h4' fontWeight='bold' mb={2} sx={{color:'white'}}>
-                New <span style={{ color: '#FC1503' }}>videos</span>
+                {selectedCategory} <span style={{ color: '#FC1503' }}>videos</span>
             </Typography>
 
-            <Videos />
+            <Videos videos= {[videos]} />
         </Box>
     </Stack>
   )
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import { Stack, Box } from '@mui/material'
 
-const Videos = () => {
+const Videos = ({ videos }) => {
+  console.log(videos);
   return (
-    <div>Videos</div>
+    <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
+        videos
+    </Stack>
   )
 }
 
